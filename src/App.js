3c13759ed05fe86ecb0009236031eef35fbfb91e
Marine FRANCESCHI <franceschi.marine@gmail.com>
2017-09-12 21:33:29 +0200

    add fake data in initial state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,31 @@ import './App.css';
 const buttons = [15, 16, 17, 18, 19, 20, 'ø'];
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      teams: [
+        {
+          players: ['Thomas', 'Marine'],
+          score: {
+            15: 3,
+            16: 2
+          }
+        },
+
+        {
+          players: ['John', 'David'],
+
+          score: {
+            15: 3,
+            16: 3,
+            17: 0
+          }
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
